dda6c56289fafd7d28d54f464d6a67af71c1542b
Kate Vorobyova <cat.vorobyova2011@yandex.ru>
2022-09-15 22:39:46 +0300

    git add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = Math.pow(b,2) - 4 * a * c;
+  if (D > 0) {
+    arr[0] = ((-b + Math.sqrt(D)) / (2 * a));
+    arr[1] = ((-b - Math.sqrt(D)) / (2 * a));
+  }
+ else if (D === 0) {
+    arr[0] = -b / (2 * a);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
