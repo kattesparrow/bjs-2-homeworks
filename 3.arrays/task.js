f0d8daf9596c53651976ed4f89ef433ab41f762f
Kate Vorobyova <cat.vorobyova2011@yandex.ru>
2022-09-20 22:48:38 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((num, i) => num === arr2[i]);
+  } else result = false;
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);;
 
-  // Ваш код
+
 
   return resultArr; // array
 }
