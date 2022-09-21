4b9fee06893fe3223c9889f5abfb5a9ebdb98ff0
Kate Vorobyova <cat.vorobyova2011@yandex.ru>
2022-09-21 17:06:54 +0300

    decoratots8.1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,59 @@
+// Задача 1. Усовершенствуйте кэширующий декоратор
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return function wrapper(...rest) {
+    const key = rest.join(",");
+    let index = cache.findIndex((element) => element.key === key);
+    if (index !== -1) {
+      console.log("Из кэша: " + cache[index].value);
+      return "Из кэша: " + cache[index].value;
+    }
+    let result = func(...rest);
+    cache.push({ key: key, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
+}
+
+// Задача 2.  Debounce декоратор с моментальным вызовом и подсчётом количества вызовов
+
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let flag = true;
+  return function wrapper(...rest) {
+    if (flag) {
+      func(...rest);
+      flag = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+      flag = true;
+    }, ms);
+  };
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let flag = true;
+  function wrapper(...rest) {
+    wrapper.count += 1;
+    if (flag) {
+      func(...rest);
+      flag = false;
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...rest);
+      flag = true;
+    }, ms);
+  }
+  wrapper.count = 0;
+  wrapper.getCount = () => console.log("Количество вызовов: " + wrapper.count);
+  return wrapper;
 }
